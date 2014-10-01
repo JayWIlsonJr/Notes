3222ea1198fa69ed3789584e002a5f2216561e5f
Jacob Smith <jacob@jacobsmith.io>
2014-10-01 15:56:10 -0400

    Finish book app

--- 10-01-2014/library/app/scripts/main.js
+++ 10-01-2014/library/app/scripts/main.js
@@ -1,3 +1,22 @@
+var Book = Backbone.Model.extend({});
+
+var BookCollection = Backbone.Collection.extend({
+  model: Book
+});
+
+var LibraryListView = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'library-list',
+  initialize: function(){
+    // Append/prepend to parent container
+    $('.jumbotron').append(this.el);
+
+    this.listenTo(this.collection, 'add', function(book){
+      this.$el.append('<li>' + book.get('title') + '</li>');
+    });
+  }
+});
+
 var CreateBookView = Backbone.View.extend({
   tagName: "input",
   className: "create-book",
@@ -11,16 +30,36 @@ var CreateBookView = Backbone.View.extend({
 
   addToLibrary: function(event){
     if(event.keyCode === 13){
-       $('.library-list').append('<li>' + this.$el.val() + '</li>');
+      var book = this.collection.add({title: this.$el.val()});
     }
   },
 
   render: function(){
     $('.jumbotron').prepend(this.el);
   }
+});
+
+var BookSummaryView = Backbone.View.extend({
+  initialize: function(){
+    $('.jumbotron').append(this.el);
+    this.listenTo(this.collection, 'add', function(){
+      this.render();
+    });
+  },
+
+  render: function(){
+    this.$el.html('<h2>' + this.collection.length + '</h2>');
+  }
 })
 
 $(document).ready(function(){
-   var createBookView = new CreateBookView();
-   createBookView.render();
+  var books = new BookCollection();
+
+  var createBookView = new CreateBookView({collection: books});
+  createBookView.render();
+
+  var libraryListView = new LibraryListView({collection: books});
+
+  var summaryView = new BookSummaryView({collection: books});
+  summaryView.render();
 });
