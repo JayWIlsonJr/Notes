aa968171e7c10f4f32b539d409350b6c9e5d20a6
Jacob Smith <jacob@jacobsmith.io>
2014-10-02 12:22:54 -0400

    Finish library app

--- 10-01-2014/library/app/scripts/main.js
+++ 10-01-2014/library/app/scripts/main.js
@@ -1,18 +1,49 @@
 /* global Backbone, _, $ */
 'use strict';
 
-var Book = Backbone.Model.extend({});
+var Book = Backbone.Model.extend({
+  defaults: {
+    title: "",
+    author: "",
+    isCheckedOut: false
+  },
+  firebase: new Backbone.Firebase("https://blistering-torch-3318.firebaseio.com/")
+});
 
-var BookCollection = Backbone.Collection.extend({
-  model: Book
+var BookCollection = Backbone.Firebase.Collection.extend({
+  model: Book,
+  firebase: "https://blistering-torch-3318.firebaseio.com/"
 });
 
 var BookView = Backbone.View.extend({
   tagName: 'li',
-  template: _.template('<input type="checkbox"><%= title %>'),
+  template: _.template('<input type="checkbox"><%= author %>: <%= title %><button>X</button>'),
+
+  initialize: function(){
+    this.listenTo(this.model, 'destroy', this.remove);
+  },
+
+  events: {
+    'change': 'toggleCheckedOut',
+    'click button': 'destroyBook'
+  },
+
+  destroyBook: function(){
+    console.log(this.model);
+    this.model.destroy();
+  },
+
+  toggleCheckedOut: function(e){
+    var isChecked = $(e.target).is(':checked');
+    this.model.set('isCheckedOut', isChecked);
+    this.model.save();
+  },
 
   render: function(){
     this.$el.html(this.template(this.model.attributes));
+    if(this.model.get('isCheckedOut')) {
+      this.$('input').attr('checked', 'checked');
+    }
   }
 });
 
@@ -26,17 +57,25 @@ var LibraryListView = Backbone.View.extend({
     // Append/prepend to parent container
     this.$container.append(this.el);
 
-    this.listenTo(this.collection, 'add', function(book){
-      var bookView = new BookView({ model: book });
-      bookView.render();
-      // this is doing the same thing as $container.append inside the view
-      this.$el.append(bookView.el);
-    });
+    this.listenTo(this.collection, 'sync', this.render);
+    this.listenTo(this.collection, 'add', this.renderChild);
+  },
+
+  render: function(){
+    this.$el.empty();
+    this.collection.each(_.bind(this.renderChild, this));
+  },
+
+  renderChild: function(book){
+    var bookView = new BookView({ model: book });
+    bookView.render();
+    // this is doing the same thing as $container.append inside the view
+    this.$el.append(bookView.el);
   }
 });
 
 var CreateBookView = Backbone.View.extend({
-  tagName: "input",
+  template: _.template( $('#templates-create-book').text() ),
   className: "create-book",
   attributes: {
     type: "text"
@@ -48,11 +87,14 @@ var CreateBookView = Backbone.View.extend({
 
   addToLibrary: function(event){
     if(event.keyCode === 13){
-      var book = this.collection.add({title: this.$el.val()});
+      var title = this.$('.title').val();
+      var author = this.$('.author').val();
+      var book = this.collection.create({title: title, author: author});
     }
   },
 
   render: function(){
+    this.$el.html(this.template());
     $('.jumbotron').prepend(this.el);
   }
 });
@@ -60,7 +102,7 @@ var CreateBookView = Backbone.View.extend({
 var BookSummaryView = Backbone.View.extend({
   initialize: function(){
     $('.jumbotron').append(this.el);
-    this.listenTo(this.collection, 'add', function(){
+    this.listenTo(this.collection, 'add remove', function(){
       this.render();
     });
   },
@@ -71,7 +113,7 @@ var BookSummaryView = Backbone.View.extend({
 })
 
 $(document).ready(function(){
-  var books = new BookCollection();
+  window.books = new BookCollection();
 
   var createBookView = new CreateBookView({collection: books});
   createBookView.render();
